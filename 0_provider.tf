a56cc8e437c74713c4b891d9979b38ceb14b3dac
Otuu02 <giftotuuobinna1995@gmail.com>
2025-01-13 20:59:03 +0100

    terraform deployment to gcp grp3

--- 0_provider.tf
+++ 0_provider.tf
@@ -8,16 +8,16 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("gcp-key.json") # This should match the service account key file in the pipeline
-  project     = var.project_id      # Use a variable for flexibility
-  region      = var.region          # Use a variable for region
+  #credentials = file("gcp-key.json") # This should match the service account key file in the pipeline
+  #project     = var.project_id      # Use a variable for flexibility
+  #region      = var.region          # Use a variable for region
 }
 
 #provider "google" {
-  #credentials = file("gcp-key.json") # This should match the service account key file in the pipeline
-  #project     = "sapient-spark-436013-t0"     # Use a variable for flexibility
-  #region      = "us-central1"         # Use a variable for region
-#}
+  credentials = file("gcp-key.json") # This should match the service account key file in the pipeline
+  project     = "sapient-spark-436013-t0"     # Use a variable for flexibility
+  region      = "us-central1"         # Use a variable for region
+}
 
 
 
